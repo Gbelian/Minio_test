8505da4faf39899e3887fa2b3ca822f242443864
SImonJ09 <jude-simon.agohoundje@epitech.eu>
2024-06-26 13:54:45 +0100

    minio test deploiements de la mais dzdzdz de dfdf huh de la frfr frfglk frfhy

--- monprojet/build1.sh
+++ monprojet/build1.sh
@@ -12,6 +12,9 @@ pip install -r requirements.txt
 wget https://dl.min.io/client/mc/release/linux-amd64/mc -O /tmp/mc
 chmod +x /tmp/mc
 
+sudo chmod u+rxw /tmp/mc
+sudo chown -R render. /tmp/mc
+
 # Créer des migrations de base de données basées sur les modèles
 python manage.py makemigrations
 
@@ -21,6 +24,10 @@ python manage.py migrate
 # Télécharger et configurer MinIO Server avec des identifiants par défaut
 wget https://dl.min.io/server/minio/release/linux-amd64/minio -O /tmp/minio
 chmod +x /tmp/minio
+
+sudo chmod u+rxw /tmp/minio
+sudo chown -R render. /tmp/minio
+
 
 # Démarrer MinIO Server en arrière-plan avec les identifiants par défaut
 export MINIO_ROOT_USER=minioadmin
