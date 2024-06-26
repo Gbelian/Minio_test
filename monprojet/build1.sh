1a91d113617cde086a474d6de179c6a65e208225
SImonJ09 <jude-simon.agohoundje@epitech.eu>
2024-06-26 12:43:17 +0100

    minio test deploiements de la mais dzdzdz de dfdf huh de la frfr

--- monprojet/build1.sh
+++ monprojet/build1.sh
@@ -28,11 +28,11 @@ export MINIO_ROOT_PASSWORD=minioadmin
 nohup /tmp/minio server /data &
 
 # Attendre que MinIO démarre
-sleep 10
+#sleep 10
 
 # Configurer MinIO Client (mc) avec les mêmes identifiants
-/tmp/mc alias set myminio http://127.0.0.1:9000 minioadmin minioadmin
-/tmp/mc mb myminio/data
+#/tmp/mc alias set myminio http://127.0.0.1:9000 minioadmin minioadmin
+#/tmp/mc mb myminio/data
 
 # Collecte des fichiers statiques
 python manage.py collectstatic --no-input
